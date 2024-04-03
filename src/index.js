fcbf4cfcaa9ee26f1399a912ee8cc512517b93e2
Xin Deng <xind1470@gmail.com>
2024-04-03 15:49:39 -0700

    added footer and menu component

--- src/index.js
+++ src/index.js
@@ -47,20 +47,41 @@ const pizzaData = [
 ];
 
 function App() {
-  return ( <div>
-    <h1>Hello React</h1>
-    <Pizza />
-    <Pizza />
-    <Pizza />
-    </div>);
-}
-
-function Pizza(){
   return (
     <div>
-    <img src='pizzas/spinaci.jpg' alt='Pizza spinaci'/>
-    <h2>Pizza Spinaci</h2>
-    <p>Tomato, mozarella, spinach, and ricotta</p>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta</p>
     </div>
   );
 }
