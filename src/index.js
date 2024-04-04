f72491de9df1959de1b50c81756912c55ee9784a
Xin Deng <xind1470@gmail.com>
2024-04-03 17:19:38 -0700

    added conditional rendering to footer and menu components

--- src/index.js
+++ src/index.js
@@ -69,18 +69,21 @@ function Header() {
   );
 }
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      {/* use a truthy condition for short circuiting */}
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ): <p>We're still working on our menu. Please come back later:)</p>}
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza)=>(
-          <Pizza pizzaObj={pizza} key={pizza.name}/>
-        ))}
-      </ul>
-
-
-{/* 
+      {/* 
       <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta"
@@ -107,7 +110,6 @@ function Pizza(props) {
         <p>{props.pizzaObj.ingredients}</p>
         <span></span>
       </div>
-
     </li>
   );
 }
@@ -124,7 +126,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      ):<p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>}
     </footer>
   );
 }
